82b34595ea9e4be1eb1ac62507eebbfb5fd08577
aditya-vetale <adityavetale7028@gmail.com>
2022-08-10 20:05:04 +0530

    Added nav functionality

--- src/sections/about/about.js
+++ src/sections/about/about.js
@@ -1,1 +1,5 @@
-History;
+import React from "react";
+
+export const About = () => {
+  return <div id="about">About</div>;
+};
---
+++ src/sections/nav/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <div id="nav">
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+      <a href="#experience">Experience</a>
+      <a href="#footer">Footer</a>
+      <a href="#projects">Projects</a>
+    </div>
+  );
+};
