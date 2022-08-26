224102014b5926f42be5dc6bbd3715fc6557e7a9
aditya-vetale <adityavetale7028@gmail.com>
2022-08-26 23:14:17 +0530

    Nav bar keeps selected until another option is selected

--- src/sections/nav/nav.js
+++ src/sections/nav/nav.js
@@ -1,13 +1,57 @@
-import React from "react";
-
+// import React from "react";
+import { useEffect, useState } from "react";
+import "./style.css";
 export const Nav = () => {
+  const [section, setSection] = useState(null);
+
+  function keepHover(event) {
+    console.log("event.target-", event.target);
+    setSection(event.target.id);
+    console.log("section-", section);
+  }
+  console.log("section outside-", section);
   return (
     <div id="nav">
-      <a href="#about">About</a>
-      <a href="#contact">Contact</a>
-      <a href="#experience">Experience</a>
-      <a href="#footer">Footer</a>
-      <a href="#projects">Projects</a>
+      <a
+        id="aboutNav"
+        className={section === "aboutNav" ? "effect" : ""}
+        href="#about"
+        onClick={(event) => keepHover(event)}
+      >
+        About
+      </a>
+      <a
+        id="contactNav"
+        className={section === "contactNav" ? "effect" : ""}
+        href="#contact"
+        onClick={(event) => keepHover(event)}
+      >
+        Contact
+      </a>
+      <a
+        id="experienceNav"
+        className={section === "experienceNav" ? "effect" : ""}
+        href="#experience"
+        onClick={(event) => keepHover(event)}
+      >
+        Experience
+      </a>
+      <a
+        className={section === "footerNav" ? "effect" : ""}
+        id="footerNav"
+        href="#footer"
+        onClick={(event) => keepHover(event)}
+      >
+        Footer
+      </a>
+      <a
+        id="projectsNav"
+        className={section === "projectsNav" ? "effect" : ""}
+        href="#projects"
+        onClick={(event) => keepHover(event)}
+      >
+        Projects
+      </a>
     </div>
   );
 };
